32cea13179cf40d8ca8faeb75cc4d11a9b66372f
BonusTrack02 <wlsgud321@gmail.com>
2022-04-22 17:07:49 +0900

    added google map on MapFragment added circleimageview library

--- app/src/main/java/com/bonustrack02/parktp/MapFragment.kt
+++ app/src/main/java/com/bonustrack02/parktp/MapFragment.kt
@@ -1,14 +1,34 @@
 package com.bonustrack02.parktp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.bonustrack02.parktp.databinding.FragmentMapBinding
+import com.google.android.gms.maps.SupportMapFragment
 
 class MapFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val fragmentManager : FragmentManager = requireActivity().supportFragmentManager
+        val mapFragment : SupportMapFragment? = fragmentManager.findFragmentById(R.id.gmap) as? SupportMapFragment
+
+        mapFragment?.getMapAsync {
+
+        }
+
+        var permissions : MutableList<String> = mutableListOf(Manifest.permission.ACCESS_FINE_LOCATION)
+        if (requireContext().checkSelfPermission(permissions[0]) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(permissions.toTypedArray(), 100)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
