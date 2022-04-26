2c2cdca7b266874f90aab6fbdff49020910600c7
BonusTrack02 <wlsgud321@gmail.com>
2022-04-26 15:46:00 +0900

    added google login service

--- app/src/main/java/com/bonustrack02/parktp/LoginActivity.kt
+++ app/src/main/java/com/bonustrack02/parktp/LoginActivity.kt
@@ -3,20 +3,60 @@ package com.bonustrack02.parktp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bonustrack02.parktp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
 
     val binding : ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    lateinit var firebaseAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val intent = Intent(this, MainActivity::class.java)
+
         binding.loginBtnNonlogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+        binding.loginBtnGoogle.setOnClickListener {
+            clickGoogle()
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun clickGoogle() {
+        val idToken = getString(R.string.google_id_token)
+        val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(idToken)
+            .requestEmail()
+            .build()
+
+        val intent = GoogleSignIn.getClient(this, signInOptions).signInIntent
+        resultLauncher.launch(intent)
+    }
+
+    val resultLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val intent = it.data
+        val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(intent)
+
+        var account = task.result
+        val email = account.email
+
     }
 }
--- app/src/main/java/com/bonustrack02/parktp/MapFragment.kt
+++ app/src/main/java/com/bonustrack02/parktp/MapFragment.kt
@@ -1,32 +1,75 @@
 package com.bonustrack02.parktp
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.bonustrack02.parktp.databinding.FragmentMapBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment() {
+
+    val binding : FragmentMapBinding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+    var lat : Double = 0.0
+    var lng : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val fragmentManager : FragmentManager = requireActivity().supportFragmentManager
         val mapFragment : SupportMapFragment? = fragmentManager.findFragmentById(R.id.gmap) as? SupportMapFragment
 
-        mapFragment?.getMapAsync {
-
-        }
-
         var permissions : MutableList<String> = mutableListOf(Manifest.permission.ACCESS_FINE_LOCATION)
         if (requireContext().checkSelfPermission(permissions[0]) == PackageManager.PERMISSION_DENIED) {
             requestPermissions(permissions.toTypedArray(), 100)
         }
+
+        providerClient = LocationServices.getFusedLocationProviderClient(activity as MainActivity)
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        locationRequest.interval = 3000
+
+        providerClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+
+        mapFragment?.getMapAsync(object : OnMapReadyCallback {
+            @SuppressLint("MissingPermission")
+            override fun onMapReady(p0: GoogleMap) {
+                var myLocation = LatLng(lat, lng)
+                Log.i("latlng", "$lat , $lng")
+                p0.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 16f))
+
+                var marker = MarkerOptions()
+                marker.title("내 위치")
+                marker.position(myLocation)
+                marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_24))
+                marker.anchor(0.5f, 1.5f)
+
+                p0.addMarker(marker)
+
+                val settings = p0.uiSettings
+                settings.isZoomControlsEnabled = true
+                p0.isMyLocationEnabled = true
+            }
+        })
+
     }
 
     override fun onCreateView(
@@ -37,8 +80,9 @@ class MapFragment : Fragment() {
         return binding.root
     }
 
-    val binding : FragmentMapBinding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+    lateinit var providerClient : FusedLocationProviderClient
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -52,5 +96,21 @@ class MapFragment : Fragment() {
             tran.addToBackStack(null)
             tran.commit()
         }
+    }
+
+    val locationCallback : LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            super.onLocationResult(p0)
+
+            val location : Location = p0.lastLocation
+            lat = location.latitude
+            lng = location.longitude
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (providerClient != null) providerClient.removeLocationUpdates(locationCallback)
     }
 }
